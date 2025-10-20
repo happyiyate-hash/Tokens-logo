8cea45b060cc4c96df4b34d537f045f3fd87b0be
Good luck Steven <goodlucksteven676@gmail.com>
2025-10-20 13:24:27 +0000

    Error: ./src/app/(user) You cannot have two parallel pages that resolve

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -1,3 +1,4 @@
+
 // This page is no longer needed as the root now redirects to /dashboard.
 // The content was part of the user dashboard.
 // This file is being removed to resolve the parallel route conflict.
